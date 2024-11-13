e363a8eadc0ce042208424fa4b3ae3d233217247
HYBRIDDDD <141039731+HYBRIDDDD@users.noreply.github.com>
2024-11-13 16:32:43 +0100

    working on about us

--- ncdc_project/ncdc/models.py
+++ ncdc_project/ncdc/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    excerpt = models.TextField(max_length=300, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='blog_images/', blank=True, null=True)  # New field for image
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']  # Order by most recent
