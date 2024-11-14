7c4e87951678682dd3f01349cf48600c857a52d6
HYBRIDDDD <141039731+HYBRIDDDD@users.noreply.github.com>
2024-11-14 13:42:11 +0100

    working on about us

--- ncdc_project/ncdc/urls.py
+++ ncdc_project/ncdc/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('about', about, name='about'),
-    path('about/deparments', deparments, name='departments')
+    path('', views.index, name='home'),
+    path('about', views.about, name='about'),
+    path('deparments', views.deparments, name='departments'),
+    path('leadership/', views.leadership, name='leadership'),  # Add this if needed
+    path('office-of-the-dg/', views.office_of_dg, name='office_of_dg'),
 ]
--- ncdc_project/ncdc/views.py
+++ ncdc_project/ncdc/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from .models import Blog
 
+
 # Create your views here.
 def index(request):
     return render(request, 'ncdc/index.html')
+
 
 def about(request):
     blogs = Blog.objects.all()[:3]
     return render(request, 'ncdc/about.html', {'blogs': blogs})
 
+
 def deparments(request):
     return render(request, 'ncdc/departments.html')
+
+
+def leadership(request):
+    return render(request, 'leadership.html')
+
+
+def office_of_dg(request):
+    return render(request, 'office_of_dg.html')
