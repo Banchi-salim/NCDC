2443699323ebe0d82da489eeca4e0b9082dca143
HYBRIDDDD <141039731+HYBRIDDDD@users.noreply.github.com>
2024-11-14 13:30:13 +0100

    working on about us

--- ncdc_project/ncdc/urls.py
+++ ncdc_project/ncdc/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', about, name='about'),
-    path('deparments/', deparments, name='departments')
+    path('about', about, name='about'),
+    path('about/deparments', deparments, name='departments')
 ]
