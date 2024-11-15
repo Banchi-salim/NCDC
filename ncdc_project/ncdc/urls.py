68ca9588063ef1372d8875ddbdf40bed6d0e491d
HYBRIDDDD <141039731+HYBRIDDDD@users.noreply.github.com>
2024-11-15 11:59:01 +0100

    working on about us

--- ncdc_project/ncdc/urls.py
+++ ncdc_project/ncdc/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('deparments', views.deparments, name='departments'),
-    path('leadership/', views.heads_of_departments, name='leadership'),  # Add this if needed
+    path('leadership', views.heads_of_departments, name='leadership'),  # Add this if needed
     path('dg/', views.office_of_dg, name='office_of_dg'),
 ]
