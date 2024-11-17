873e0615777ffaedd62e98e25ee7ae02801af709
HYBRIDDDD <141039731+HYBRIDDDD@users.noreply.github.com>
2024-11-17 23:43:14 +0100

    create an alert on site visit

--- ncdc_project/ncdc/urls.py
+++ ncdc_project/ncdc/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('deparments', views.deparments, name='departments'),
     path('leadership', views.heads_of_departments, name='leadership'),  # Add this if needed
     path('dg/', views.office_of_dg, name='office_of_dg'),
-    path("update-location/", views.update_location, name="update_location"),
+    path("update-location/", views.update_location, name="update_location")
 ]
