a19d89a58797218ccd0156bb9cc5fca21d6a8e92
HYBRIDDDD <141039731+HYBRIDDDD@users.noreply.github.com>
2024-12-03 00:39:28 +0100

    making all publications pages

--- ncdc_project/ncdc_project/settings.py
+++ ncdc_project/ncdc_project/settings.py
@@ -108,6 +108,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'usmanabdulsalim@gmail.com'
+EMAIL_HOST_PASSWORD = '080254@Salim60'
+DEFAULT_FROM_EMAIL = 'usmanabdulsalim@gmail.com'
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
