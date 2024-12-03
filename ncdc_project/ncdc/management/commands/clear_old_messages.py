71d067466e0f1cbb41ecd3390724b065e1caed15
HYBRIDDDD <141039731+HYBRIDDDD@users.noreply.github.com>
2024-12-03 02:05:16 +0100

    making all publications pages

---
+++ ncdc_project/ncdc/management/commands/clear_old_messages.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from ncdc_project.ncdc.models import ChatMessage
+from datetime import timedelta
+from django.utils.timezone import now
+
+class Command(BaseCommand):
+    help = "Delete chat messages older than 24 hours"
+
+    def handle(self, *args, **kwargs):
+        cutoff = now() - timedelta(hours=24)
+        deleted_count, _ = ChatMessage.objects.filter(timestamp__lt=cutoff).delete()
+        self.stdout.write(f"{deleted_count} old messages cleared.")
