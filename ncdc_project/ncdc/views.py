023832ea2ab30ed3f531820f8d35e579b816f8eb
HYBRIDDDD <141039731+HYBRIDDDD@users.noreply.github.com>
2024-12-10 07:49:57 +0100

    added accounts for donate page

--- ncdc_project/ncdc/views.py
+++ ncdc_project/ncdc/views.py
@@ -64,7 +64,7 @@ def chatbot_api(request):
 
 def index(request):
     events = Events.objects.all()[:3]
-    return render(request, 'ncdc/index.html', {'context':events})
+    return render(request, 'ncdc/index.html', {'events':events})
 
 
 def about(request):
